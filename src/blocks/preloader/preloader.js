214c24357247d4ab1c648ae419a3cbfb146d10d9
_tango <shurinskiy@mail.ru>
2022-09-28 12:34:45 +0600

    add preloader

--- src/blocks/preloader/preloader.js
+++ src/blocks/preloader/preloader.js
@@ -2,10 +2,10 @@
 
 	window.onload = function() {
 		let preloader = document.querySelector('.preloader');
-		preloader.classList.add('preloader_hide');
 
-		setInterval(function() {
-			preloader.classList.add('preloader_hidden');
+		preloader?.classList.add('preloader_hide');
+		setInterval(() => {
+			preloader?.classList.add('preloader_hidden');
 		}, 990);
 	}
 
